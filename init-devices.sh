c4753a61bb3f633e525aad46be7ebd97f9371121
Maximilian Bischoff <maximilian.bischoff@inovex.de>
2019-02-01 12:06:24 +0100

    Removed scripts

--- init-devices.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-HUB_NAME=$1
-for iotdevice in edge-device-1 edge-device-2; do
-   az iot hub device-identity create -n $HUB_NAME -d $iotdevice
-   echo "Created $iotdevice. Connection String:"
-   az iot hub device-identity show-connection-string -n $HUB_NAME -d $iotdevice | jq ".cs"
-done
--- init-node.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-HUB_NAME=$1
-az iot hub device-identity create -n $HUB_NAME -d edge-node --edge-enabled
-az iot hub device-identity show-connection-string -n $HUB_NAME -d edge-node | jq ".cs"
